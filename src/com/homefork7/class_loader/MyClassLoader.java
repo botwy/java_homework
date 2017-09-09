b18322e1cdeacad271e3ddc09705357a5f43aa04
botwy <botwy@mail.ru>
2017-09-09 13:45:22 +0300

    PluginManager #1

---
+++ src/com/homefork7/class_loader/MyClassLoader.java
@@ -0,0 +1,5 @@
+package com.homefork7.class_loader;
+
+public class MyClassLoader extends ClassLoader {
+
+}
---
+++ src/com/homefork7/class_loader/PluginManager.java
@@ -0,0 +1,15 @@
+package com.homefork7.class_loader;
+
+public class PluginManager {
+
+        private final String pluginRootDirectory;
+
+        public PluginManager(String pluginRootDirectory) {
+            this.pluginRootDirectory = pluginRootDirectory;
+        }
+
+        public Plugin load(String pluginName, String pluginClassName) {
+            //todo
+        }
+
+}
