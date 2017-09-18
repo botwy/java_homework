e0e4d6c8d9179c5c86460652739ffa82e8004c22
botwy <botwy@mail.ru>
2017-09-18 16:53:12 +0300

    Sgu2056 #1

--- src/com/sgu2056/Solution.java
+++ src/com/sgu2056/Solution.java
@@ -7,40 +7,44 @@ import java.util.TreeSet;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String text;
-        text = scanner.nextLine();
-       // text += scanner.nextLine();
+String data = "dd D a Cc aa aaa aaaa\n" +
+        " a Dd aa d aaa cC bbbb\n"+
+        " a Dd aa d aaa   cC bbbb bbbb aaaC aaaC aaaC\n";
+        Scanner scanner = new Scanner(data);
+        String text = "";
+        while (scanner.hasNextLine())
+            text += scanner.nextLine();
+
         String[] arr = text.split("[\\n \\r \\t]+");
         HashMap<String, Integer> hashMap_word = new HashMap<String, Integer>();
         TreeSet<String> treeSet_word = new TreeSet<String>();
 
         for (String word : arr
                 ) {
-            word=word.toLowerCase();
-            //       System.out.println(word);
-            //list_word.add(word);
+            word = word.toLowerCase();
+
+
             if (hashMap_word.containsKey(word)) {
                 Integer new_value = hashMap_word.get(word) + 1;
                 hashMap_word.put(word, new_value);
             } else hashMap_word.put(word, 1);
         }
 
-        int max_item=0;
+        int max_item = 0;
         for (Map.Entry<String, Integer> item : hashMap_word.entrySet()
                 ) {
-            System.out.println(item.getKey() + " - " + item.getValue());
-            if (item.getValue()>max_item) max_item=item.getValue();
+
+            if (item.getValue() > max_item) max_item = item.getValue();
         }
-        System.out.println(max_item);
+
 
         for (Map.Entry<String, Integer> item : hashMap_word.entrySet()
                 ) {
-            if (item.getValue()==max_item) treeSet_word.add(item.getKey());
+            if (item.getValue() == max_item) treeSet_word.add(item.getKey());
         }
 
-        for (String str:treeSet_word
-             ) {
+        for (String str : treeSet_word
+                ) {
             System.out.println(str);
         }
 
